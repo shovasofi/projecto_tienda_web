d94818ba98c6393b23872dd3a104507185251dc9
shovasofi <90852177+shovasofi@users.noreply.github.com>
2023-02-24 20:55:01 +0100

    Add files via upload

--- tienda/tienda.js
+++ tienda/tienda.js
@@ -36,7 +36,7 @@ function call_ajax() {
                     var objeto_json = JSON.parse(jsonResponse);
                     //acceder a las propiedades
                     objeto_json.codigo_producto
-                    resolve(this.codigo)
+                    resolve(objeto_json);
                 } else {
                     reject();
                 }
@@ -54,5 +54,78 @@ Promise.all(call_ajax())
     .then(e => console.log("todo ok "))
     .catch(e => console.log("error"));
 
+console.log("hola")
+
+//----------------------------------
+
+const buscarInput = document.querySelector('#descripcion');
+const listaSugerencias = document.querySelector('#sugerencias');
+
+buscarInput.addEventListener('input', descripcion);
+
+function descripcion() {
+    const textoBuscado = buscarInput.value;
+    if (textoBuscado.trim().length > 0) {
+        getSugerencias(textoBuscado)
+            .then(mostrarSugerencias)
+            .catch(gestionarError);
+    } else {
+        limpiarSugerencias();
+    }
+}
+
+function getSugerencias(textoBuscado) {
+    return new Promise((resolve, reject) => {
+        // Peticion asincrona para obtener sugerencias y resolver la promesa con
+        // los resultados. Aqui se devuelven sugerencias estáticas.
+        //const sugerencias = ['Prod1', 'Prrod2', 'Pprod3', 'Clara', 'IES'];
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "productos.json", true);
+        xhttp.send();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    //buscar dentro del archivo json productos 
+                    var jsonResponse = xhttp.responseText;
+                    //pasarlo a objeto
+                    var objeto_json = JSON.parse(jsonResponse);
+                    //acceder a las propiedades
+                    var sugerencias = objeto_json.descripcion;
 
 
+                    const sugerenciasFiltradas = sugerencias.filter(sugerencia =>
+                        sugerencia.toLowerCase().startsWith(textoBuscado.toLowerCase()));
+                    resolve(sugerenciasFiltradas);
+                } else {
+                    reject();
+                }
+            }
+
+        }
+
+        
+    });
+
+}
+
+Promise.all(getSugerencias())
+    .then(e => console.log("todo ok "))
+    .catch(e => console.log("error"));
+
+function mostrarSugerencias(sugerencias) {
+    listaSugerencias.innerHTML = '';
+    sugerencias.forEach(sugerencia => {
+        const li = document.createElement('li');
+        li.textContent = sugerencia;
+        listaSugerencias.appendChild(li);
+    });
+}
+
+function gestionarError(error) {
+    console.error(error);
+}
+
+function limpiarSugerencias() {
+    listaSugerencias.innerHTML = '';
+}
